53d6cfea0321acbdfee60b5c43f1624fa6567967
haoling <maya@fei-yen.jp>
2018-01-03 22:11:37 +0900

    リンク先が画像のときは、リンク先のURLをピンする

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function pinIt(info, tab) {
     chrome.tabs.sendMessage(tab.id, {method: "getSelection"}, function () {
       chrome.storage.local.get('pinryUrl', function (obj) {
+        console.log(info);
         console.log(obj);
+        var srcUrl = info.srcUrl;
+        if (info.linkUrl.match(/\.(jpe?g|png|gif)$/)) srcUrl = info.linkUrl;
         var description = tab.title.substr(0, 500);
-        var url = obj.pinryUrl + "/pins/pin-form/?pin-image-url=" + encodeURIComponent(info.srcUrl) +
+        var url = obj.pinryUrl + "/pins/pin-form/?pin-image-url=" + encodeURIComponent(srcUrl) +
           "&pin-description=" + encodeURIComponent(tab.title + "\n" + tab.url) +
           "&referer=" + encodeURIComponent(tab.url);
           chrome.windows.create({
