985d16c9821e5b13786b1e0ee772223b4e5ce977
Haoling <maya@fei-yen.jp>
2020-12-09 20:37:23 +0900

    cookie対応

--- main.js
+++ main.js
@@ -6,15 +6,21 @@ function pinIt(info, tab) {
         var srcUrl = info.srcUrl;
         if (info.linkUrl != undefined && info.linkUrl.match(/\.(jpe?g|png|gif)$/)) srcUrl = info.linkUrl;
         var description = tab.title.substr(0, 500);
-        var url = obj.pinryUrl + "/pins/pin-form/?pin-image-url=" + encodeURIComponent(srcUrl) +
-          "&pin-description=" + encodeURIComponent(tab.title + "\n" + tab.url) +
-          "&referer=" + encodeURIComponent(tab.url);
-          chrome.windows.create({
-              url: url,
-              type: "popup",
-              width: 784,
-              height: 800
-          });
+        var parser = new URL(srcUrl);
+        console.log(parser);
+        chrome.cookies.getAll({"url":parser.protocol + "//" + parser.host}, function(cookies) {
+          console.log(cookies);
+          var url = obj.pinryUrl + "/pins/pin-form/?pin-image-url=" + encodeURIComponent(srcUrl) +
+            "&pin-description=" + encodeURIComponent(tab.title + "\n" + tab.url) +
+            "&referer=" + encodeURIComponent(tab.url) +
+            "&cookie=" + encodeURIComponent(cookies.map(function(c){ return c.name + "=" + c.value }).join("; "));
+            chrome.windows.create({
+                url: url,
+                type: "popup",
+                width: 784,
+                height: 800
+            });
+        });
       });
     });
 }
